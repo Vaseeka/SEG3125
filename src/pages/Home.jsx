e3f3fb2ea7e20086a959f83ca18fa4e765ae3dab
Vaseeka <vaseeka.bala@gmail.com>
2026-05-18 19:55:02 -0400

    Added placeholder for all 4 project cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,40 @@
 import Project from '../components/Project';
 import serviceImg from '../assets/service.jpg';
+import memoryImg from '../assets/memory-game.jpg';
+import ecommerceImg from '../assets/e-commerce.jpg';
+import analyticsImg from '../assets/analytics.jpg';
 
 function Home() {
   return (
     <div className="container">
-        <div class="text-center display-2"><code>Vaseeka Balachandiran</code></div>
-        <div class="text-center display-6"><code>Software Engineer</code></div>
+        <div className="text-center display-2"><code>Vaseeka Balachandiran</code></div>
+        <div className="text-center display-6"><code>Software Engineer</code></div>
         <div className="container my-4 border">
             <div className="row">
-                <div class="text-center align-self-center display-4 col-4">About Me</div>
-                <div class="text-start lead col-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, distinctio quod corporis magni amet asperiores temporibus. Quas culpa voluptatem eum saepe ratione? Quaerat maiores dolorum corrupti voluptatibus quae quibusdam ratione.</div>
+                <div className="text-center align-self-center display-4 col-4">About Me</div>
+                <div className="text-start lead col-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, distinctio quod corporis magni amet asperiores temporibus. Quas culpa voluptatem eum saepe ratione? Quaerat maiores dolorum corrupti voluptatibus quae quibusdam ratione.</div>
             </div>
             
         </div>
         <div className="container my-3 border">
             <div className="row">
                 
-                <div class="text-start lead col-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, distinctio quod corporis magni amet asperiores temporibus. Quas culpa voluptatem eum saepe ratione? Quaerat maiores dolorum corrupti voluptatibus quae quibusdam ratione.</div>
-                <div class="text-center align-self-center display-4 col-4">How I Work</div>
+                <div className="text-start lead col-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, distinctio quod corporis magni amet asperiores temporibus. Quas culpa voluptatem eum saepe ratione? Quaerat maiores dolorum corrupti voluptatibus quae quibusdam ratione.</div>
+                <div className="text-center align-self-center display-4 col-4">How I Work</div>
             </div>   
         </div>
 
 
-        <div className="row mt-4">
+        <h2 className="mt-5 text-center">Case Studies</h2>
 
-            <Project title="Service Site" image={serviceImg}/>
+        <div className="row mt-3">
 
-      </div>
+            <Project title="Service Site" image={serviceImg} />
+            <Project title="Memory Game" image={memoryImg} />
+            <Project title="E-Commerce Site" image={ecommerceImg} />
+            <Project title="Analytics Dashboard" image={analyticsImg} />
+
+        </div>
     </div>
   );
 }
