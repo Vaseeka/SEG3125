d163507f691e50db3b503ff2a593566e17d2864e
Vaseeka <vaseeka.bala@gmail.com>
2026-05-18 19:27:43 -0400

    Created basic layout for homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,43 @@
 function Home() {
   return (
     <div className="container">
-        <h1>Home Page</h1>
-        <div className="container my-3 border">
+        <div class="text-center display-2"><code>Vaseeka Balachandiran</code></div>
+        <div class="text-center display-6"><code>Software Engineer</code></div>
+        <div className="container my-4 border">
             <div className="row">
-                <div class="text-center align-self-center display-2 col-sm-4">About Me</div>
-                <div class="text-start lead col-sm-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, distinctio quod corporis magni amet asperiores temporibus. Quas culpa voluptatem eum saepe ratione? Quaerat maiores dolorum corrupti voluptatibus quae quibusdam ratione.</div>
+                <div class="text-center align-self-center display-4 col-4">About Me</div>
+                <div class="text-start lead col-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, distinctio quod corporis magni amet asperiores temporibus. Quas culpa voluptatem eum saepe ratione? Quaerat maiores dolorum corrupti voluptatibus quae quibusdam ratione.</div>
             </div>
             
         </div>
         <div className="container my-3 border">
             <div className="row">
                 
-                <div class="text-start lead col-sm-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, distinctio quod corporis magni amet asperiores temporibus. Quas culpa voluptatem eum saepe ratione? Quaerat maiores dolorum corrupti voluptatibus quae quibusdam ratione.</div>
-                <div class="text-center align-self-center display-2 col-sm-4">How I Work</div>
+                <div class="text-start lead col-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, distinctio quod corporis magni amet asperiores temporibus. Quas culpa voluptatem eum saepe ratione? Quaerat maiores dolorum corrupti voluptatibus quae quibusdam ratione.</div>
+                <div class="text-center align-self-center display-4 col-4">How I Work</div>
+            </div>
+            
+        </div>
+
+        <div className="container">
+            <div className="row">
+                <div class="text-center lead col-6 my-3 border">Service Site</div>
+                <div class="text-center lead col-6 my-3 border">Memory Game</div>
+            </div>
+            <div className="row">
+                <div class="text-center lead col-6 border">Service Site Img</div>
+                <div class="text-center lead col-6 border">Memory Game Img</div>
+            </div>
+            <div className="row">
+                <div class="text-center align-self-center display-4 border">Case Studies</div>
+            </div>
+            <div className="row">
+                <div class="text-center lead col-6 border">E-Commerce Site Img</div>
+                <div class="text-center lead col-6 border">Analytics Site Img</div>
+            </div>
+            <div className="row">
+                <div class="text-center lead col-6 my-3 border">E-Commerce Site</div>
+                <div class="text-center lead col-6 my-3 border">Analytics Site</div>
             </div>
             
         </div>
