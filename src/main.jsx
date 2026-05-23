e7a2c89d98be1e1e8a3fb54a190610a150db9e77
Vaseeka <vaseeka.bala@gmail.com>
2026-05-23 12:33:30 -0400

    Restore point in case code breaks

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+/* 
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -11,3 +27,5 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>
 )
+
+*/
